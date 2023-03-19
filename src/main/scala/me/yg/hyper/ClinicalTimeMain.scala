231ac9e7b03869fcd0a706884af27ad63a5de70d
ygkim <gonni21c@gmail.com>
2023-03-19 22:41:01 +0900

    daily commit

--- src/main/scala/me/yg/hyper/ClinicalTimeMain.scala
+++ src/main/scala/me/yg/hyper/ClinicalTimeMain.scala
@@ -39,14 +39,16 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import org.deeplearning4j.nn.conf.inputs.InputType
+import org.nd4j.evaluation.classification.Evaluation
 
+import scala.jdk.CollectionConverters._
 
 object ClinicalTimeMain extends App {
   println("Active Training ..")
 
-  val DATA_PATH = "/Users/a1000074/dev/sample/"
+  val DATA_PATH = "/Users/ygkim/dev/sample/"
 
-  val NB_TRAIN_EXAMPLES = 3200 // number of training examples
+  val NB_TRAIN_EXAMPLES = 1 //3200 // number of training examples
   val NB_TEST_EXAMPLES = 800 // number of testing examples
 
   // init data
@@ -66,13 +68,24 @@ object ClinicalTimeMain extends App {
   val trainData = new SequenceRecordReaderDataSetIterator(trainFeatures, trainLabels,
     32, 2, false, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END)
 
-  trainData.next().asList().forEach(a => println(a))
+
   println("-----------------------------")
+  println(trainLabels.next().get(0).toString)
 
+//  trainDt.asList().asScala.map(ds => ds.asList().forEach(println))
+//  trainFeatures.next().asScala.foreach(println)
 
-//  while(trainData.hasNext) {
-//
-//  }
+  println("--------------ds---------------")
+//  println("==========>" + trainData.next().asList().size())
+//  trainData.next().asScala.foreach(println)
+  trainData.next().asScala.foreach(ds => {
+    println("row ===========================")
+    ds.get(0).asList().asScala.foreach(col => {
+      println("col =============")
+      col.toString
+    })
+  })
+
 
   // Load testing data
   val testFeatures = new CSVSequenceRecordReader(1, ",");
@@ -83,56 +96,66 @@ object ClinicalTimeMain extends App {
 
   val testData = new SequenceRecordReaderDataSetIterator(testFeatures, testLabels,
     32, 2, false, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
-//
-//  // net configuration
-//  // Set neural network parameters
-//  val NB_INPUTS = 86
-//  val NB_EPOCHS = 10
-//  val RANDOM_SEED = 1234
-//  val LEARNING_RATE = 0.005
-//  val BATCH_SIZE = 32
-//  val LSTM_LAYER_SIZE = 200
-//  val NUM_LABEL_CLASSES = 2
-//
-//  val conf = new NeuralNetConfiguration.Builder()
-//    .seed(RANDOM_SEED)
-//    .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-//    .updater(new Adam(LEARNING_RATE))
-//    .weightInit(WeightInit.XAVIER)
-//    .dropOut(0.25)
-//    .graphBuilder()
-//    .addInputs("trainFeatures")
-//    .setOutputs("predictMortality")
-//    .addLayer("L1", new GravesLSTM.Builder()
-//      .nIn(NB_INPUTS)
-//      .nOut(LSTM_LAYER_SIZE)
-//      .forgetGateBiasInit(1)
-//      .activation(Activation.TANH)
-//      .build(),
-//      "trainFeatures")
-//    .addLayer("predictMortality", new RnnOutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
-//      .activation(Activation.SOFTMAX)
-//      .nIn(LSTM_LAYER_SIZE)
-//      .nOut(NUM_LABEL_CLASSES)
-//      .build(), "L1")
-//    .setOutputs("predictMortality")
-//    .setInputTypes(InputType.recurrent(86))
-//    .build()
-//
-//  val model = new ComputationGraph(conf)
-//
-//  // Training
-//  model.fit(trainData, 2)
-//
-//  // Model Evaluation
-//  val roc = new ROC(100);
-//
-//  while (testData.hasNext()) {
-//    val batch = testData.next();
-//    val output = model.output(batch.getFeatures());
-//    roc.evalTimeSeries(batch.getLabels(), output(0));
-//  }
-//
-//  println("FINAL TEST AUC: " + roc.calculateAUC());
 
+  // ----------------
+  processing
+
+  def processing() = {
+    // net configuration
+    // Set neural network parameters
+    val NB_INPUTS = 86 // 시간 포함 시간당 측정 항목, 총 86건
+    val NB_EPOCHS = 10
+    val RANDOM_SEED = 1234
+    val LEARNING_RATE = 0.005
+    val BATCH_SIZE = 32
+    val LSTM_LAYER_SIZE = 200
+    val NUM_LABEL_CLASSES = 2
+
+    val conf = new NeuralNetConfiguration.Builder()
+      .seed(RANDOM_SEED)
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+      .updater(new Adam(LEARNING_RATE))
+      .weightInit(WeightInit.XAVIER)
+      .dropOut(0.25)
+      .graphBuilder()
+      .addInputs("trainFeatures")
+      .setOutputs("predictMortality")
+      .addLayer("L1", new GravesLSTM.Builder()
+        .nIn(NB_INPUTS)
+        .nOut(LSTM_LAYER_SIZE)
+        .forgetGateBiasInit(1)
+        .activation(Activation.TANH)
+        .build(),
+        "trainFeatures")
+      .addLayer("predictMortality", new RnnOutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
+        .activation(Activation.SOFTMAX)
+        .nIn(LSTM_LAYER_SIZE)
+        .nOut(NUM_LABEL_CLASSES)
+        .build(), "L1")
+      .setOutputs("predictMortality")
+      .setInputTypes(InputType.recurrent(86))
+      .build()
+
+
+
+    // Training
+//    val model = new ComputationGraph(conf)
+//    model.fit(trainData, 2)
+//    model.save(new File("model/clinic.mdl"))
+
+    // Load
+    val model = ComputationGraph.load(new File("model/clinic.mdl"), false)
+
+    // Model Evaluation
+//    val roc = new ROC(100);
+//    while (testData.hasNext()) {
+//      val batch = testData.next();
+//      val output = model.output(batch.getFeatures());
+//      roc.evalTimeSeries(batch.getLabels(), output(0));
+//    }
+//    println("FINAL TEST AUC: " + roc.calculateAUC());
+
+    println("Eval => " + model.evaluate(testData))
+  }
 }
+
